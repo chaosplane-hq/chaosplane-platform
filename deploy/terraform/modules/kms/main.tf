370f05793450661345fbb94b01dcba372ded5ba1
Juan Lee <me@juany.kr>
2026-06-02 00:20:49 +0900

    feat(infra): add KMS CMK module

---
+++ deploy/terraform/modules/kms/main.tf
@@ -0,0 +1,65 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    sid = "EnableRootAccountAccess"
+
+    actions = [
+      "kms:*",
+    ]
+
+    resources = [
+      "*",
+    ]
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
+      ]
+    }
+  }
+
+  statement {
+    sid = "AllowServiceEncryption"
+
+    actions = [
+      "kms:Decrypt",
+      "kms:DescribeKey",
+      "kms:Encrypt",
+      "kms:GenerateDataKey",
+      "kms:GenerateDataKeyWithoutPlaintext",
+      "kms:ReEncryptFrom",
+      "kms:ReEncryptTo",
+    ]
+
+    resources = [
+      "*",
+    ]
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "ec2.amazonaws.com",
+        "logs.${data.aws_region.current.name}.amazonaws.com",
+        "rds.amazonaws.com",
+        "s3.amazonaws.com",
+        "secretsmanager.amazonaws.com",
+      ]
+    }
+  }
+}
+
+resource "aws_kms_key" "this" {
+  deletion_window_in_days = 30
+  enable_key_rotation     = true
+  policy                  = data.aws_iam_policy_document.this.json
+  tags                    = var.tags
+}
+
+resource "aws_kms_alias" "this" {
+  name          = "alias/${var.name}"
+  target_key_id = aws_kms_key.this.key_id
+}
